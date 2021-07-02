d30848c1a749c3634bb40675d910d9ff743fc71f
Rodrigo Lima De Moraes <rodrigo.moraes@mercadolivre.com>
2021-07-02 15:43:07 -0300

    add not null validation

--- src/main/java/com/mercadolibre/bootcamp_g1_final_project/controller/request/LoginRequest.java
+++ src/main/java/com/mercadolibre/bootcamp_g1_final_project/controller/request/LoginRequest.java
@@ -1,7 +1,11 @@
 package com.mercadolibre.bootcamp_g1_final_project.controller.request;
 
+import javax.validation.constraints.NotNull;
+
 public class LoginRequest {
+    @NotNull(message = "Email cant be null")
     private String email;
+    @NotNull(message = "Password cant be null")
     private String password;
 
     public String getEmail() {
